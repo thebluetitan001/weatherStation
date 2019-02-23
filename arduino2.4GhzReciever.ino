44d1b632c855faacfa63ece71e2a091428844767
thebluetitan001 <47161028+thebluetitan001@users.noreply.github.com>
2019-02-23 15:47:50 +1100

    Update arduino2.4GhzReciever.ino

--- arduino2.4GhzReciever.ino
+++ arduino2.4GhzReciever.ino
@@ -11,29 +11,32 @@ int counter =0;
 void setup()
 {
 // Start the serial
-Serial.begin(9600);
-Serial.println("NRF24 Receiver");
-radio.begin();
-radio.setPALevel(RF24_PA_LOW); // Transmit Power (MAX,HIGH,LOW,MIN)
-radio.setChannel(0x76);
-radio.setDataRate( RF24_1MBPS ); //Transmit Speeed (250 Kbits)
-radio.openReadingPipe(1,0xF0F0F0F0E1LL);
-radio.startListening();
+  Serial.begin(9600);
+  Serial.println("NRF24 Receiver");
 
-pinMode(6, OUTPUT);
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW); // Transmit Power (MAX,HIGH,LOW,MIN)
+  radio.setChannel(0x76);
+  radio.setDataRate( RF24_1MBPS ); //Transmit Speeed (250 Kbits)
+  radio.openReadingPipe(1,0xF0F0F0F0E1LL);
+  radio.startListening();
+
+  pinMode(6, OUTPUT);
 
 }
 
 
 void loop()
 {
-if (radio.available())
-{
-char text[100] = {0}; //Buffer
-radio.read(&text, sizeof(text));
-received_data = String(text);
-Serial.println(received_data);
-Serial.println(counter);
-counter++;
-} 
+  if (radio.available()){    {
+    char text[100] = {0}; //Buffer
+    
+    radio.read(&text, sizeof(text));
+    received_data = String(text);
+    
+    Serial.println(received_data);
+    Serial.println(counter);
+    
+    counter++;
+  } 
 }
